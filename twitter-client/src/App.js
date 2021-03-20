87b9b4d5f53714e7b665d5b02df5f705cde6b92b
Niveditha Kani <nivedithakani@gmail.com>
2021-03-20 19:24:38 -0400

    Add empty project

--- twitter-client/src/App.js
+++ twitter-client/src/App.js
@@ -1,12 +1,7 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import TweetsPage from "./TweetsPage.js";
-
 function App() {
 	return (
 		<>
-			<ChakraProvider>
-				<TweetsPage />
-			</ChakraProvider>
+			Hello World
 		</>
 	);
 }
--- twitter-client/src/Tweet.js
+++ twitter-client/src/Tweet.js
@@ -1,12 +1,7 @@
-import { Box, Heading } from "@chakra-ui/react";
-
-function Tweet(props) {
-	const { username, content } = props;
+function Tweet() {
 	return (
-		<Box p={5} m={3} bg="blue.100" borderRadius="full" textAlign="center">
-			<Heading>{username}</Heading>
-			{content}
-		</Box>
+		<>
+		</>
 	);
 }
 
